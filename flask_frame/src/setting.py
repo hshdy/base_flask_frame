dfacf6814eeb51edb7dbbe68881586a3cb4bb9a7
huangshihua <huangshihua@yihaohulian.com>
2021-07-01 16:38:11 +0800

    chore:添加依赖包版本文件

--- flask_frame/src/setting.py
+++ flask_frame/src/setting.py
@@ -11,21 +11,21 @@
 
 class __Setting:
             
-    MYSQL_MAX_OVERFLOW = int()  # 64
-    REDIS_HOST = str()  # 192.168.1.188
-    MONGO_USERNAME = None  # None
-    REDIS_PORT = int()  # 6379
-    MONGO_PORT = int()  # 27017
-    MYSQL_POOL_RECYCLE = int()  # 3600
-    LISTEN_PORT = int()  # 7998
-    MONGO_HOST = str()  # 192.168.1.188
-    MYSQL_POOL_SIZE = int()  # 64
     DEBUG_METHOD = int()  # 1
-    MYSQL_URL = str()  # mysql+pymysql://root:mysql@192.168.1.188/flask_frame_db
+    LISTEN_PORT = int()  # 7998
     TEST_IS_CHANGE = int()  # 0
-    REDIS_PASSWORD = None  # None
+    MYSQL_MAX_OVERFLOW = int()  # 64
+    MYSQL_POOL_RECYCLE = int()  # 3600
+    MYSQL_POOL_SIZE = int()  # 64
+    MYSQL_URL = str()  # mysql+pymysql://root:mysql@192.168.1.188/flask_frame_db
     MONGO_PASSWORD = None  # None
+    MONGO_HOST = str()  # 192.168.1.188
+    MONGO_USERNAME = None  # None
+    MONGO_PORT = int()  # 27017
+    REDIS_HOST = str()  # 192.168.1.188
+    REDIS_PORT = int()  # 6379
     REDIS_DB = int()  # 0
+    REDIS_PASSWORD = None  # None
     MAX_REDIS_CONNECTION = None  # None
 
 SETTING = __Setting()
